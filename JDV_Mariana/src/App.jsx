e86057e5368318001258a2c1995d9c91536fe09e
Mariana Rodrigues <mariana.s.rodrigues13@aluno.senai.br>
2026-08-27 09:01:27 -0300

    MOD: Finalizando o board

--- JDV_Mariana/src/App.jsx
+++ JDV_Mariana/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import { Square } from './components/Square/Square'
 
 function App() {
-  <>
+  return(
+    <>
   <Square></Square>
 
   </>
+  )
+  
 }
 
 export default App
--- JDV_Mariana/src/components/Board/Board.jsx
+++ JDV_Mariana/src/components/Board/Board.jsx
@@ -10,4 +10,24 @@ export function Board({squares, onPlay, winningLine}){
     const IsWinning = (index) =>{
         return winningLine ? winningLine.includes(index) : false;
     }
+
+    return(
+        <>
+        <div className={styles['board']}
+        >
+            {squares.map((value, index) => (
+                <Square
+                key={index}
+                value={value}
+                OnSquareClick={() => onPlay(index)}
+                IsWinning={IsWinning(index)}
+                >
+
+                </Square>
+            ))}
+
+
+        </div>
+        </>
+    )
 }
